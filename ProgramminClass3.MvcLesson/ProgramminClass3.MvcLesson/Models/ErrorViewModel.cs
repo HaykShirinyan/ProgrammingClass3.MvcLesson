63d995620bf8e676f06b1e6fd0e7c91f59403095
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2022-12-07 05:27:11 -0800

    Created the solution

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgramminClass3.MvcLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
