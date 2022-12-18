562457e15ceba331c8ff6736bb247a98ba45b4ad
Alex <alexaperge@yahoo.com>
2022-12-17 23:56:41 -0800

    Added - About page

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/HomeController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace ProgramminClass3.MvcLesson.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
