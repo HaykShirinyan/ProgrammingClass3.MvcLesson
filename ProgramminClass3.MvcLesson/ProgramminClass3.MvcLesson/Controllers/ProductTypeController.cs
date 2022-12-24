1fa45cb12b83dafaf953e9654da49f56823e7357
martzuro <arturgevorgyan1131@gmail.com>
2022-12-25 00:04:43 +0400

    add producttype model

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ProductTypeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramminClass3.MvcLesson.Data;
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductTypesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var ProductTypes = _dbContext.ProductTypes.ToList();
+
+            return View(ProductTypes);
+        }
+    }
+}
---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Models/ProductType.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramminClass3.MvcLesson.Models
+{
+    public class ProductType
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+
+}
+
