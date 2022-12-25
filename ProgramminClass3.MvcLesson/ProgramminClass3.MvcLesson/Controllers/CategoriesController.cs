3e3a32be8b56c2815a319e48acf64e196642b848
Alex <alexaperge@yahoo.com>
2022-12-25 10:38:20 -0800

    Added Categories, ProductType and Unit OfMeasures Tables

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramminClass3.MvcLesson.Data;
+using ProgramminClass3.MvcLesson.Models;
+
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();
+
+            return View(categories); 
+        }
+    }
+}
---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Models/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramminClass3.MvcLesson.Models
+{
+    public class ProductType
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+    }
+}
