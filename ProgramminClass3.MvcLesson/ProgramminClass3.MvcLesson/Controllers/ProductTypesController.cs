77845411d87c51181bf9298473de9bcff1f9a477
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-01-06 00:09:57 +0400

    Revert "still try"
    This reverts commit 71cbfe910844ce64d4ae8b2bf81ba729ea2779bb.

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ProductTypesController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ProductTypesController.cs
@@ -12,21 +12,13 @@ namespace ProgramminClass3.MvcLesson.Controllers
         {
             _dbContext = dbContext;
         }
-        [HttpGet]
+
         public IActionResult Index()
         {
             var productTypes = _dbContext.ProductTypes.ToList();
 
             return View(productTypes);
         }
-
-        [HttpGet]
-        public IActionResult Create() 
-        {
-            return View();
-        }
-
-
 
     }
 }
