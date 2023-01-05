73df1be03c4cf392d6a8dd8c543aac913902daf6
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-01-06 02:10:17 +0400

    Categories controller done

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoriesController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoriesController.cs
@@ -13,12 +13,53 @@ namespace ProgramminClass3.MvcLesson.Controllers
         {
             _dbContext = dbContext;
         }
-
+        [HttpGet]
         public IActionResult Index()
         {
             var categories = _dbContext.Categories.ToList();
 
             return View(categories); 
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
     }
 }
