d98890bb5d740b55622723cca21aaaa286939db6
martzuro <arturgevorgyan1131@gmail.com>
2023-01-06 00:18:15 +0400

    added create and edit pages

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoryController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProgramminClass3.MvcLesson.Data;
+using ProgramminClass3.MvcLesson.Data.Migrations;
+using ProgramminClass3.MvcLesson.Models;
 
 namespace ProgramminClass3.MvcLesson.Controllers
 {
@@ -16,6 +18,50 @@ namespace ProgramminClass3.MvcLesson.Controllers
             var categories = _dbContext.Categories.ToList();
 
             return View(categories);
+        }
+
+        [HttpGet]       
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+               return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _dbContext.Categories.Find(id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
     }
 }
