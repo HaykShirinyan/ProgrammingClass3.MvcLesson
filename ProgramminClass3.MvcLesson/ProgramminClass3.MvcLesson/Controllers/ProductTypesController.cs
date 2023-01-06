f2d1665cdef1927de4cc436320cb0bcc05a05850
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-01-06 16:17:56 +0400

    added ProductTypes create and edit

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ProductTypesController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ProductTypesController.cs
@@ -13,6 +13,7 @@ namespace ProgramminClass3.MvcLesson.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             var productTypes = _dbContext.ProductTypes.ToList();
@@ -20,5 +21,47 @@ namespace ProgramminClass3.MvcLesson.Controllers
             return View(productTypes);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create (ProductType productType)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.ProductTypes.Add(productType);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(productType);
+        }
+
+        [HttpGet]
+        public IActionResult Edit (int id)
+        {
+            var productTypes = _dbContext.ProductTypes.Find(id);
+            return View(productTypes);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit (ProductType productType)
+        {
+            if (ModelState.IsValid) 
+            {
+                _dbContext.ProductTypes.Update(productType);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(productType);
+        }
     }
 }
