0c806c3c277da0127a4f9f9386b8974a3a096e62
eduardgevorgyan06 <117510728+eduardgevorgyan06@users.noreply.github.com>
2023-01-07 23:06:31 +0400

    added edit and add functions

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/UnitOfMeasuresController.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/UnitOfMeasuresController.cs
@@ -13,11 +13,55 @@ namespace ProgramminClass3.MvcLesson.Controllers
         {
             _dbContext = dbContext;
         }
+
         public IActionResult Index()
         {
             var unitOfMeasures = _dbContext.UnitOfMeasures.ToList();
             
             return View(unitOfMeasures);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(unitOfMeasure);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+            return View(unitOfMeasure);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Update(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(unitOfMeasure);
         }
     }
 }
