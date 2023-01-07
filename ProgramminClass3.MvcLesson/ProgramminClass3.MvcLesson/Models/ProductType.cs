27d7a251a7bfdf6d82339c36912458512cf5ef28
Aleyan Davit <davaleyan@mail.ru>
2023-01-07 18:39:18 +0400

    models

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Models/ProductType.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramminClass3.MvcLesson.Models
+{
+    public class ProductType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
