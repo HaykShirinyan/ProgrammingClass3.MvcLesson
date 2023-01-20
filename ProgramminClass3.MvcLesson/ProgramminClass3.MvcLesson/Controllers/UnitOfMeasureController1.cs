399a8635a85b087448e981a4daa13f15e830613c
Gohar Alaverdyan <goharalaverdyan94@gmail.com>
2023-01-20 00:13:25 -0800

    changed

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/UnitOfMeasureController1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramminClass3.MvcLesson.Data;
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class UnitOfMeasureController1 : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public UnitOfMeasureController1(ApplicationDbContext dbContext)
+
+        { 
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.ToList();
+            return View(unitOfMeasure);
+        }
+    }
+}
