bbf5c0136779337dd4a90f217f9e5d407f16d794
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-01-22 12:30:56 -0800

    Added ProductCategories many-to-many relationship

--- ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Data/Migrations/20230122194939_ProductCategories.Designer.cs
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Data/Migrations/20230122194939_ProductCategories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProgramminClass3.MvcLesson.Data;
 
@@ -11,9 +12,10 @@ using ProgramminClass3.MvcLesson.Data;
 namespace ProgramminClass3.MvcLesson.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230122194939_ProductCategories")]
+    partial class ProductCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -291,7 +293,7 @@ namespace ProgramminClass3.MvcLesson.Data.Migrations
 
                     b.HasIndex("CategoryId");
 
-                    b.ToTable("ProductCategories");
+                    b.ToTable("ProductCategory");
                 });
 
             modelBuilder.Entity("ProgramminClass3.MvcLesson.Models.ProductType", b =>
