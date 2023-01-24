bd31ae9d7209f129aa301e3c7ca8aecde3cc8451
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-01-24 12:09:42 +0400

    Added ColosController with Create and Edit actions

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/ColorsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramminClass3.MvcLesson.Data;
+using ProgramminClass3.MvcLesson.Models;
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class ColorsController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ColorsController (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var colors = _dbContext.Colors.ToList();
+
+            return View(colors);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Color color)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Colors.Add(color);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(color);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var color = _dbContext.Colors.Find(id);
+            return View(color);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Color color)
+        {
+            if(ModelState.IsValid)
+            {
+                _dbContext.Colors.Update(color);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(color);
+        }
+    }
+}
