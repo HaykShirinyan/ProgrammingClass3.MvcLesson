1fdd0f615229bb39311630493890370c157533d0
Gohar Alaverdyan <goharalaverdyan94@gmail.com>
2023-01-29 10:11:37 -0800

    h

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Data/Migrations/20230129092624_UnitOfMeasuresId.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgramminClass3.MvcLesson.Data.Migrations
+{
+    public partial class UnitOfMeasuresId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UnitOfMeasuresId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UnitOfMeasuresId",
+                table: "Products",
+                column: "UnitOfMeasuresId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_UnitOfMeasures_UnitOfMeasuresId",
+                table: "Products",
+                column: "UnitOfMeasuresId",
+                principalTable: "UnitOfMeasures",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_UnitOfMeasures_UnitOfMeasuresId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UnitOfMeasuresId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UnitOfMeasuresId",
+                table: "Products");
+        }
+    }
+}
