c5372a9581ec5388a9e2c86e05ab9516943fadab
Gohar Alaverdyan <goharalaverdyan94@gmail.com>
2023-01-28 18:36:19 -0800

    h

---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgramminClass3.MvcLesson.Data;
+using ProgramminClass3.MvcLesson.Models;
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public CategoriesController (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var categories = _dbContext.Categories.ToList();
+            return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Add(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductTypes = _dbContext.ProductTypes.ToList();
+
+            return View(category);
+        }
+
+
+
+    }
+}
---
+++ ProgramminClass3.MvcLesson/ProgramminClass3.MvcLesson/Controllers/UnitOfMeasuresController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgramminClass3.MvcLesson.Data;
+
+namespace ProgramminClass3.MvcLesson.Controllers
+{
+    public class UnitOfMeasuresController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public UnitOfMeasuresController(ApplicationDbContext dbContext)
+
+        { 
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.ToList();
+            return View(unitOfMeasure);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UnitOfMeasure unitOfMeasure)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductTypes = _dbContext.ProductTypes.ToList();
+
+            return View(unitOfMeasure);
+        }
+    }
+}
